758fd198a5f72e27a44cfe21edcba6746fdcd1b2
rookie-leo <proleonardopa@gmail.com>
2025-12-03 21:54:10 -0300

    Initial configuration

---
+++ src/main/kotlin/com/rookie_leo/pessoa/PessoaApplication.kt
@@ -0,0 +1,11 @@
+package com.rookie_leo.pessoa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PessoaApplication
+
+fun main(args: Array<String>) {
+	runApplication<PessoaApplication>(*args)
+}
