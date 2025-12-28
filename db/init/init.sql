df494242966d94aababe8fb78cf133b0986a47d6
rookie-leo <proleonardopa@gmail.com>
2025-12-28 15:15:53 -0300

    Database configuration

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,5 +1,13 @@
 USE blog-pessoas;
 
+CREATE TABLE IF NOT EXISTS TB_PESSOAS (
+    id_pessoa CHAR(36) PRIMARY KEY,
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    documento VARCHAR(20),
+    senha VARCHAR(100)
+);
+
 INSERT INTO TB_PESSOAS (id_pessoa, nome, email, documento, senha)
 VALUES
     (UUID(), 'Fulano', 'fulano@email.com', '41584719079', '123abc'),
