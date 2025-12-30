5084b384670fb10aecd0e8336f292b77fd0e7d48
rookie-leo <proleonardopa@gmail.com>
2025-12-30 19:06:29 -0300

    Database configuration

--- db/init/init.sql
+++ db/init/init.sql
@@ -1,15 +1,17 @@
 USE blog-pessoas;
 
-CREATE TABLE IF NOT EXISTS TB_PESSOAS (
-    id_pessoa CHAR(36) PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS tb_pessoas (
+    id_pessoa BINARY(16) NOT NULL UNIQUE,
     nome VARCHAR(100),
-    email VARCHAR(100),
-    documento VARCHAR(20),
-    senha VARCHAR(100)
+    email VARCHAR(100) UNIQUE,
+    documento VARCHAR(20) UNIQUE,
+    senha VARCHAR(100),
+    PRIMARY KEY (id_pessoa)
 );
 
-INSERT INTO TB_PESSOAS (id_pessoa, nome, email, documento, senha)
+INSERT INTO tb_pessoas (id_pessoa, nome, email, documento, senha)
 VALUES
-    (UUID(), 'Fulano', 'fulano@email.com', '41584719079', '123abc'),
-    (UUID(), 'Sicrano', 'sicrano@email.com', '33794861086', 'abc123'),
-    (UUID(), 'Beltrano', 'beltrano@email.com', '10773370005', '321cba');
+    (UUID_TO_BIN(UUID()), 'Fulano', 'fulano@email.com', '41584719079', '123abc'),
+    (UUID_TO_BIN(UUID()), 'Sicrano', 'sicrano@email.com', '33794861086', 'abc123'),
+    (UUID_TO_BIN(UUID()), 'Beltrano', 'beltrano@email.com', '10773370005', '321cba');
+
