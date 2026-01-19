8e8399e756fa36976fd6c22cfcec485a17a3f780
rookie-leo <proleonardopa@gmail.com>
2026-01-18 22:58:06 -0300

    Implemented unit test

--- src/main/kotlin/com/rookie_leo/pessoa/core/exceptions/DatabaseException.kt
+++ src/main/kotlin/com/rookie_leo/pessoa/core/exceptions/DatabaseException.kt
@@ -2,6 +2,5 @@ package com.rookie_leo.pessoa.core.exceptions
 
 class DatabaseException(
     override val message: String,
-    override val cause: Throwable?,
     val errorCode: String? = null
 ) : RuntimeException(message)
