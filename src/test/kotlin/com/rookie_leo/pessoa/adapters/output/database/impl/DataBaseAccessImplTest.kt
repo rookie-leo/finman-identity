657d006bf798d285711116d10a2da8d53b1270e9
rookie-leo <proleonardopa@gmail.com>
2026-04-01 08:14:02 -0300

    Fixing jwt security property error

--- src/test/kotlin/com/rookie_leo/pessoa/adapters/output/database/impl/DataBaseAccessImplTest.kt
+++ src/test/kotlin/com/rookie_leo/pessoa/adapters/output/database/impl/DataBaseAccessImplTest.kt
@@ -14,7 +14,9 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 @ExtendWith(MockitoExtension::class)
 class DataBaseAccessImplTest {
 
